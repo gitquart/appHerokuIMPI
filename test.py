fd9c5f3bb0df7627bfc0e1aabd9a16707a8b5147
Ulyses <ulyses.rico.2012@gmail.com>
2020-11-09 15:52:16 -0600

    ...

--- test.py
+++
@@ -1,6 +0,0 @@
-"""
-Test.py
--Program to read from thesis.tbcourtdecisiontfjfa in datastax
--Read the base64 pdf value and decode here
--This table has text/pdf and image/pdf, so it's needed to know how are they shown decoded
-"""
