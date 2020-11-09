4a40e0addd2dc5955c3a119022b1c94810b91028
Ulyses <ulyses.rico.2012@gmail.com>
2020-11-09 15:30:09 -0600

    Adding test.py

---
+++ test.py
@@ -0,0 +1,6 @@
+"""
+Test.py
+-Program to read from thesis.tbcourtdecisiontfjfa in datastax
+-Read the base64 pdf value and decode here
+-This table has text/pdf and image/pdf, so it's needed to know how are they shown decoded
+"""
