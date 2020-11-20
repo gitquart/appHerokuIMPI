1b00211f5a267aeac8a7a372eacef1ff3cc5cafd
Ulyses <ulyses.rico.2012@gmail.com>
2020-11-20 14:52:26 -0600

    ...

--- appimpi/main.py
+++ appimpi/main.py
@@ -46,7 +46,7 @@ while(StartID<=EndID):
     if status==200:
         print('Reading page...')
         browser.get(urlExp)
-        time.sleep(1)
+        time.sleep(10)
         if browser.find_elements_by_xpath('//*[@id="divAlertas"]/div/strong').count==0:
             print('------No existe el expediente solicitado------')
             StartID=StartID+1
