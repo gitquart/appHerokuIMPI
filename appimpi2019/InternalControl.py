9d321b1a8566fdb6eb8927003f00e2d73098bde0
Ulyses <ulyses.rico.2012@gmail.com>
2020-12-21 16:53:23 -0600

    ...

--- appimpi2019/InternalControl.py
+++
@@ -1,4 +0,0 @@
-class cInternalControl:
-    idControl=8
-    timeout=70
-    hfolder='appimpi2019'     
---
+++ appimpi2020/InternalControl.py
@@ -0,0 +1,4 @@
+class cInternalControl:
+    idControl=9
+    timeout=70
+    hfolder='appimpi2020'     
