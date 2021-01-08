ed0a14092fd14f5ff710d0441fd7468c0864fc5d
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-08 16:50:46 -0600

    ...

--- appimpi2020/InternalControl.py
+++ appimpi2020/InternalControl.py
@@ -3,9 +3,10 @@ import os
 class cInternalControl:
     idControl=9
     timeout=70
-    hfolder='appimpi2020'   
+    version='2020'
+    hfolder='appimpi'+version   
     heroku=True
     rutaHeroku='/app/'+hfolder
     rutaLocal=os.getcwd()+'\\'+hfolder+'\\'
-    download_dir='Download_impi2020'
+    download_dir='Download_impi'+version
     enablePdf=False   
