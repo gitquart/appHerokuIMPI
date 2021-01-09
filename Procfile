189ce266f6c3333da33768639630f77d1abfb31a
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-08 22:24:42 -0600

    ...

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python3 /app/appimpi2015/main.py
+worker: python3 /app/appimpi2017/main.py
--- appimpi2017/InternalControl.py
+++ appimpi2017/InternalControl.py
@@ -1,9 +1,9 @@
 import os
 
 class cInternalControl:
-    idControl=3
+    idControl=6
     timeout=70
-    version='2015'
+    version='2017'
     hfolder='appimpi'+version   
     heroku=True
     rutaHeroku='/app/'+hfolder
