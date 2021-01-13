2a208a0d6c3dae7fa2b0e948180d28de75aad10e
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-13 16:00:40 -0600

    ...

--- appimpi2016/InternalControl.py
+++ appimpi2016/InternalControl.py
@@ -1,9 +1,9 @@
 import os
 
 class cInternalControl:
-    idControl=3
+    idControl=5
     timeout=70
-    version='2015'
+    version='2016'
     hfolder='appimpi'+version   
     heroku=True
     rutaHeroku='/app/'+hfolder
